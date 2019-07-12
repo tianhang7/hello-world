f2abc03e126a40af4cb84dd386584b40d86b4fd2
tianhang7 <35548801+tianhang7@users.noreply.github.com>
2019-07-12 14:44:40 -0400

    update hello world
    ask name

--- hello-world.c
+++ hello-world.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(){
 fprintf("Hello Wolrd!\n");
+fprintf("What is your name?");
+char name[100];
+scanf("%s",name);
+fprintf("Hello,%s!\n",name);
 return 0;
 }
