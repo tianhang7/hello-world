491a478964bee7b8ddb0f4428ec1102701e401e1
tianhang7 <35548801+tianhang7@users.noreply.github.com>
2019-07-12 14:48:47 -0400

    update print
    debug error

--- hello-world.c
+++ hello-world.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 int main(){
-fprintf("Hello Wolrd!\n");
-fprintf("What is your name?");
+printf("Hello Wolrd!\n");
+printf("What is your name?");
 char name[100];
 scanf("%s",name);
-fprintf("Hello,%s!\n",name);
+printf("Hello,%s!\n",name);
 return 0;
 }
